a8bbbd49be923bf33ee33c902e63b054dec2e83e
Norbert Benczúr <nbe@vizrt.com>
2018-02-26 15:04:22 +0100

    Inital boot project

---
+++ src/main/java/com/nukesz/bootpolymer/BootPolymerApplication.java
@@ -0,0 +1,12 @@
+package com.nukesz.bootpolymer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPolymerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPolymerApplication.class, args);
+	}
+}
