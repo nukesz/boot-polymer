8985e7078ba469f63b378b18281af05b84f72748
Norbert Benczur <norbert.benczur@gmail.com>
2018-02-27 22:51:13 +0100

    Read Todos from the api and present it on the page.

--- src/main/java/com/nukesz/bootpolymer/todos/boundary/TodosResource.java
+++ src/main/java/com/nukesz/bootpolymer/todos/boundary/TodosResource.java
@@ -1,19 +1,18 @@
 package com.nukesz.bootpolymer.todos.boundary;
 
 import com.nukesz.bootpolymer.todos.entity.Todo;
-
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
-@RequestMapping("/todos")
+@RequestMapping("/resources")
 public class TodosResource {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(path= "todos", method = RequestMethod.GET)
     public List<Todo> all() {
         return Arrays.asList(new Todo("todo1"), new Todo("todo2"));
     }
